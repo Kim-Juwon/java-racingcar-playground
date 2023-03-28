cb081d49278b96f128da105cce8324412452621b
김주원 <damiano102777@naver.com>
2023-03-28 16:09:39 +0900

    feat: Car 클래스 기능 테스트 성공을 위한 기능 작성
    - 정적 팩토리 메소드를 통한 객체 생성 기능
    - 위치 getter
    - 한칸 전진 시도 기능

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,4 +1,36 @@
 package domain;
 
 public class Car {
+    private static final int MINIMUM_VALUE_TO_MOVE = 4;
+
+    private final RandomValueGenerator randomValueGenerator;
+    private String name;
+    private int location;
+
+    private Car(RandomValueGenerator randomValueGenerator, String name) {
+        this.randomValueGenerator = randomValueGenerator;
+        this.name = name;
+    }
+
+    public static Car of(RandomValueGenerator randomValueGenerator, String name) {
+        return new Car(randomValueGenerator, name);
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void attemptToMove() {
+        if (meetConditionToMove()) {
+            move();
+        }
+    }
+
+    private boolean meetConditionToMove() {
+        return randomValueGenerator.generate() >= MINIMUM_VALUE_TO_MOVE;
+    }
+
+    private void move() {
+        ++location;
+    }
 }
