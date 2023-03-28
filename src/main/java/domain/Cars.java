bc3920c0d54e893c57f53d0e3c1e131444a259c4
김주원 <damiano102777@naver.com>
2023-03-28 17:39:40 +0900

    feat: Cars 클래스 기능 테스트 성공을 위한 기능 작성

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,4 +1,24 @@
 package domain;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Cars {
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars from(List<Car> cars) {
+        return new Cars(cars);
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    public void attemptToMove() {
+        cars.forEach(Car::attemptToMove);
+    }
 }
