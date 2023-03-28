980535d1182118b1396b0598425d44d1bfc53f2b
김주원 <damiano102777@naver.com>
2023-03-28 16:11:44 +0900

    feat: DigitRandomValueGenerator 클래스 구현

---
+++ src/main/java/domain/DigitRandomValueGenerator.java
@@ -0,0 +1,22 @@
+package domain;
+
+import java.util.Random;
+
+public class DigitRandomValueGenerator implements RandomValueGenerator {
+    private static final int MAX = 9;
+
+    private final Random random = new Random();
+
+    public static DigitRandomValueGenerator create() {
+        return new DigitRandomValueGenerator();
+    }
+
+    @Override
+    public int generate() {
+        return generateRandomValueInRange();
+    }
+
+    public int generateRandomValueInRange() {
+        return random.nextInt(MAX + 1);
+    }
+}
