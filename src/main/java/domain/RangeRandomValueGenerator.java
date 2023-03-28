2578cdb48fa6746722126a2aa14245e78dfe1633
김주원 <damiano102777@naver.com>
2023-03-28 16:11:29 +0900

    feat: RangeRandomValueGenerator 클래스 구현

---
+++ src/main/java/domain/RangeRandomValueGenerator.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.util.Random;
+
+public class RangeRandomValueGenerator implements RandomValueGenerator {
+    private static final int ONE = 1;
+
+    private final int min;
+    private final int max;
+    private final Random random = new Random();
+
+    private RangeRandomValueGenerator(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    /**
+     *
+     * @param min 랜덤으로 부여될 수 있는 최소 정수
+     * @param max 랜덤으로 부여될 수 있는 최대 정수
+     * @return min,max 값이 세팅된 RangeRandomValueGenerator 인스턴스
+     */
+    public static RangeRandomValueGenerator from(int min, int max) {
+        return new RangeRandomValueGenerator(min, max);
+    }
+
+    @Override
+    public int generate() {
+        return generateRandomValueInRange();
+    }
+
+    private int generateRandomValueInRange() {
+        return random.nextInt(max - min + ONE) + min;
+    }
+}
