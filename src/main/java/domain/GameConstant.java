733d8eec5652f8e9837d45d3736aba0d9a1000a9
김주원 <damiano102777@naver.com>
2023-03-29 23:28:40 +0900

    feat: GameConstant 정의

---
+++ src/main/java/domain/GameConstant.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class GameConstant {
+    public static final int MAXIMUM_DIGITS_OF_ATTEMPT_COUNT = 4;
+    public static final int MAXIMUM_VALUE_OF_ATTEMPT_COUNT = 1000;
+}
