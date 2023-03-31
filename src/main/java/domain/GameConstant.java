04dd18521c0eaab828be92f5bafeeb0838d20a14
김주원 <damiano102777@naver.com>
2023-03-31 13:49:04 +0900

    refactor: 전진 시도 횟수 최대값 변경

--- src/main/java/domain/GameConstant.java
+++ src/main/java/domain/GameConstant.java
@@ -1,6 +1,6 @@
 package domain;
 
 public class GameConstant {
-    public static final int MAXIMUM_DIGITS_OF_ATTEMPT_COUNT = 4;
-    public static final int MAXIMUM_VALUE_OF_ATTEMPT_COUNT = 1000;
+    public static final int MAXIMUM_DIGITS_OF_ATTEMPT_COUNT = 5;
+    public static final int MAXIMUM_VALUE_OF_ATTEMPT_COUNT = 10000;
 }
